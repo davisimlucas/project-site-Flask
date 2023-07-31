0777d8329cb850b1fc1c92684e53fad1ebef830a
davisimlucas <davisimoeslucas@gmail.com>
2023-07-31 19:16:49 -0300

    app py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+
 import os
 from flask import Flask, render_template 
 
 app = Flask(__name__)
 
 app.template_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)))
+app.static_folder = 'src/static'
 
 @app.route('/')
 def homepage():
