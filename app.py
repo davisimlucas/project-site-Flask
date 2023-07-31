feef38f466ce3aa750d36d8f634242af60e7b246
davisimlucas <davisimoeslucas@gmail.com>
2023-07-31 11:23:41 -0300

    app py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+import os
 from flask import Flask, render_template 
 
 app = Flask(__name__)
 
+app.template_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)))
+
 @app.route('/')
 def homepage():
-    return render_template('index.html')
+    return render_template("src/templates/homepage.html")
 
 if __name__ == "__main__":
     app.run(debug= True)
